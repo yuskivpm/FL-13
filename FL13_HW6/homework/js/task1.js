981f8961529fa4b792073f9bd0c844c20cec4aa2
Petro Yuskiv <yuskiv.pm@gmail.com>
2020-04-21 20:21:39 +0300

    feat: fl13_hw6

--- FL13_HW6/homework/js/task1.js
+++ FL13_HW6/homework/js/task1.js
@@ -1,1 +1,17 @@
-// Your code goes here
+const MAX_PERCENTAGE = 100;
+const MAX_FRACTION_DIGITS = 2;
+
+const check = parseFloat(prompt('Enter check sum'));
+const tip = parseFloat(prompt('Enter tip percentage'));
+
+if (isNaN(check) || isNaN(tip) || check < 0 || tip < 0 || tip > MAX_PERCENTAGE) {
+  alert('Invalid input data');
+} else {
+  const tipAmount = check * tip / MAX_PERCENTAGE;
+  const total = check + tipAmount;
+  alert(`Check number: ${check}\n` +
+    `Tip: ${tip}%\n` +
+    `Tip amount: ${+tipAmount.toFixed(MAX_FRACTION_DIGITS)}\n` +
+    `Total sum to pay: ${+total.toFixed(MAX_FRACTION_DIGITS)}`
+  );
+}
--- FL13_HW6/homework/js/task2.js
+++ FL13_HW6/homework/js/task2.js
@@ -1,1 +1,8 @@
-// Your code goes here
+const MAX_CHAR_COUNT = 2;
+let text = prompt('Enter word');
+
+if (text === null || (text = text.trim()).length === 0) {
+  alert('Invalid value');
+} else {
+  alert(text.substr(text.length - 1 >> 1, MAX_CHAR_COUNT - (text.length & 1)));
+}

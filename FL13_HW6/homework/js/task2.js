277e120dc3c5e7f527e4e6acaf4e0dc71f014649
Petro Yuskiv <yuskiv.pm@gmail.com>
2020-04-22 20:53:03 +0300

    refactor: fl13_hw6 - replace to const

--- FL13_HW6/homework/js/task2.js
+++ FL13_HW6/homework/js/task2.js
@@ -1,5 +1,5 @@
 const MAX_CHAR_COUNT = 2;
-let text = prompt('Enter word');
+const text = prompt('Enter word');
 
 if (text && text.trim()) {
   alert(text.substr(text.length - 1 >> 1, MAX_CHAR_COUNT - (text.length & 1)));

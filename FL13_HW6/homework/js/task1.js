30ac4311cebb19df81e4ed12e8f99eff02ebf236
Petro Yuskiv <yuskiv.pm@gmail.com>
2020-04-22 19:57:05 +0300

    fix: fl13_hw6 - after qa

--- FL13_HW6/homework/js/task1.js
+++ FL13_HW6/homework/js/task1.js
@@ -1,5 +1,4 @@
 const MAX_PERCENTAGE = 100;
-const MAX_FRACTION_DIGITS = 2;
 
 const check = parseFloat(prompt('Enter check sum'));
 const tip = parseFloat(prompt('Enter tip percentage'));
@@ -7,11 +6,10 @@ const tip = parseFloat(prompt('Enter tip percentage'));
 if (isNaN(check) || isNaN(tip) || check < 0 || tip < 0 || tip > MAX_PERCENTAGE) {
   alert('Invalid input data');
 } else {
-  const tipAmount = check * tip / MAX_PERCENTAGE;
-  const total = check + tipAmount;
+  const tipAmount = Math.round(check * tip) / MAX_PERCENTAGE;
   alert(`Check number: ${check}\n` +
     `Tip: ${tip}%\n` +
-    `Tip amount: ${+tipAmount.toFixed(MAX_FRACTION_DIGITS)}\n` +
-    `Total sum to pay: ${+total.toFixed(MAX_FRACTION_DIGITS)}`
+    `Tip amount: ${tipAmount}\n` +
+    `Total sum to pay: ${check + tipAmount}`
   );
 }
--- FL13_HW6/homework/js/task2.js
+++ FL13_HW6/homework/js/task2.js
@@ -1,8 +1,8 @@
 const MAX_CHAR_COUNT = 2;
 let text = prompt('Enter word');
 
-if (text === null || (text = text.trim()).length === 0) {
-  alert('Invalid value');
-} else {
+if (text && text.trim()) {
   alert(text.substr(text.length - 1 >> 1, MAX_CHAR_COUNT - (text.length & 1)));
+} else {
+  alert('Invalid value');
 }

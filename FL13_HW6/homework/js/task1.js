bf83bf48a67187c31e1de66899b6aedcbbfb98b0
Petro Yuskiv <yuskiv.pm@gmail.com>
2020-04-23 22:17:11 +0300

    refactor: fl13_hw6 round

--- FL13_HW6/homework/js/task1.js
+++ FL13_HW6/homework/js/task1.js
@@ -6,10 +6,11 @@ const tip = parseFloat(prompt('Enter tip percentage'));
 if (isNaN(check) || isNaN(tip) || check < 0 || tip < 0 || tip > MAX_PERCENTAGE) {
   alert('Invalid input data');
 } else {
+  const round = number => Math.round(number * MAX_PERCENTAGE) / MAX_PERCENTAGE;
   const tipAmount = Math.round(check * tip) / MAX_PERCENTAGE;
-  alert(`Check number: ${check}\n` +
-    `Tip: ${tip}%\n` +
+  alert(`Check number: ${round(check)}\n` +
+    `Tip: ${round(tip)}%\n` +
     `Tip amount: ${tipAmount}\n` +
-    `Total sum to pay: ${check + tipAmount}`
+    `Total sum to pay: ${round(check + tipAmount)}`
   );
 }

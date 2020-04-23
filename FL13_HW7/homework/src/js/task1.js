b4d30cee8419c02a6382d279f6b0fd17ed1ffc75
Petro Yuskiv <yuskiv.pm@gmail.com>
2020-04-24 00:36:58 +0300

    feat: fl13_hw7

--- FL13_HW7/homework/src/js/task1.js
+++ FL13_HW7/homework/src/js/task1.js
@@ -1,1 +1,24 @@
-// Your code goes here
+const MIN_LOGIN_LENGTH = 4;
+const EVENING_HOURS = 20;
+
+const usersDB = { 'User': 'UserPass', 'Admin': 'RootPass' };
+
+const userLogin = prompt('Enter login');
+
+if (!userLogin) {
+  alert('Canceled.');
+} else if (userLogin.length < MIN_LOGIN_LENGTH) {
+  alert('I don\'t know any users having name length less than 4 symbols');
+} else if (!usersDB.hasOwnProperty(userLogin)) {
+  alert('I don\'t know you');
+} else {
+  const userPassword = prompt('Enter password');
+  if (!userPassword) {
+    alert('Canceled.');
+  } else if (usersDB[userLogin] !== userPassword) {
+    alert('Wrong password');
+  } else {
+    const hour = new Date().getHours();
+    alert(`Good ${hour < EVENING_HOURS ? 'day' : 'evening'}, dear ${userLogin}!`);
+  }
+}
--- FL13_HW7/homework/src/js/task2.js
+++ FL13_HW7/homework/src/js/task2.js
@@ -1,1 +1,51 @@
-// Your code goes here
+if (!confirm('Do you want to play a game?')) {
+  alert('You did not become a billionaire, but can.');
+} else {
+
+  const ATTEMPTS_COUNT = 3;
+  const START_PRIZE_NUMBER = 5;
+  const START_PRIZE_AMOUNT = 100;
+
+  let roundPrizeNumber;
+  let roundMaxNumber;
+  let roundMaxPossiblePrize;
+  let roundPossiblePrize;
+  let roundAttemptsCount;
+  let totalPrize;
+
+  do {
+    totalPrize = 0;
+    roundMaxNumber = START_PRIZE_NUMBER;
+    roundMaxPossiblePrize = START_PRIZE_AMOUNT;
+
+    do {
+      roundPossiblePrize = roundMaxPossiblePrize;
+      roundAttemptsCount = ATTEMPTS_COUNT;
+      roundPrizeNumber = Math.trunc(Math.random() * (roundMaxNumber + 1));
+      while (
+        roundPrizeNumber !== Number(prompt(
+          `Choose a roulette pocket number from 0 to ${roundMaxNumber}\n` +
+          `Attempts left: ${roundAttemptsCount}\n` +
+          `Total prize: ${totalPrize}$\n` +
+          `Possible prize on current attempt: ${roundPossiblePrize}$`))
+        && --roundAttemptsCount
+      ) {
+        roundPossiblePrize >>= 1;
+      }
+
+      if (roundAttemptsCount) {
+        totalPrize += roundPossiblePrize;
+        if (confirm(`Congratulation, you won!\n` +
+          `Your prize is ${roundPossiblePrize}$\n` +
+          `Do you want to continue?`)) {
+          roundMaxNumber += START_PRIZE_NUMBER;
+          roundMaxPossiblePrize <<= 1;
+        } else {
+          roundAttemptsCount = 0;
+        }
+      }
+    } while (roundAttemptsCount);
+
+    alert(`Thank you for your participation. your prize is: ${totalPrize}$`);
+  } while (confirm('Do you want to continue?'));
+}

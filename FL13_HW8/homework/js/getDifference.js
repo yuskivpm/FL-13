6926b281135886970a1ba4ad03021ad217419483
Petro Yuskiv <yuskiv.pm@gmail.com>
2020-04-28 19:49:31 +0300

    refactor: remove comments

--- FL13_HW8/homework/js/getDifference.js
+++ FL13_HW8/homework/js/getDifference.js
@@ -1,4 +1,4 @@
 const getDifference = (a, b) => a > b ? a - b : b - a;
 
-console.log(getDifference(5, 3)); // => 2
-console.log(getDifference(5, 8)); // => 3
+console.log(getDifference(5, 3));
+console.log(getDifference(5, 8)); 

9f38b209401dda3946eb3ef1b58507c8cc00bf9f
Petro Yuskiv <yuskiv.pm@gmail.com>
2020-05-20 18:38:42 +0300

    refactor: fl13_hw14 after qa

--- FL13_HW14/homework/js/app.js
+++ FL13_HW14/homework/js/app.js
@@ -2,31 +2,30 @@ function Student(name, email) {
   const homeworkResult = [];
   this.getName = () => name;
   this.getEmail = () => email;
-  this.addHomeworkResult = (topic, success) => {
-    homeworkResult.push({ topic, success })
-  };
   this.getHomeworkResults = () => homeworkResult.slice();
   this.toString = () => `name: ${name}, email: ${email}`;
+  this.addHomeworkResult = (topic, success) => {
+    homeworkResult.push({ topic, success });
+  };
 }
 
 function FrontendLab(students, failedHomeworksLimit) {
   const studentsList = students.map(({ name, email }) => new Student(name, email));
-  this.printStudentsList = () => studentsList.forEach(student => {
-    console.log(student.toString());
-    console.log(student.getHomeworkResults());
-  });
-  this.addHomeworkResults = ({ topic, results }) => results.forEach(
-    ({ email, success }) => {
+  const isEligibleForTest = homeworkResults => homeworkResults.reduce((total, { success }) => total + !success, 0);
+  this.printStudentsList = () =>
+    studentsList.forEach(student => {
+      console.log(student.toString());
+      console.log(student.getHomeworkResults());
+    });
+  this.addHomeworkResults = ({ topic, results }) =>
+    results.forEach(({ email, success }) => {
       const student = studentsList.find(student => student.getEmail() === email);
       if (student) {
         student.addHomeworkResult(topic, success);
       }
-    }
-  );
-  this.printStudentsEligibleForTest = () => studentsList
-    .filter(
-      student => failedHomeworksLimit >= student.getHomeworkResults()
-        .reduce((total, hwr) => total + !hwr.success, 0)
-    )
-    .forEach(student => console.log(student.toString()));
+    });
+  this.printStudentsEligibleForTest = () =>
+    studentsList
+      .filter(student => failedHomeworksLimit >= isEligibleForTest(student.getHomeworkResults()))
+      .forEach(student => console.log(student.toString()));
 }
